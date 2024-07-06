a644d577b4f4a075b2c2b823cbbb3b527a5b14f7
samya-roy <samyaroy999@gmail.com>
2024-07-07 00:55:53 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import { StickyNavbar } from "./Components/Navbar/Navbar";
+function App() {
+  return (
+    <>
+      <div className="bg-black">
+        <StickyNavbar />
+      </div>
+    </>
+  )
+}
+
+export default App

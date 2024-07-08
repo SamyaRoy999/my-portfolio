d26631124eaf7696587bb6510953a2bdeb210cf1
samya-roy <samyaroy999@gmail.com>
2024-07-09 01:41:32 +0600

    project details added

--- src/Components/Data/Data.js
+++ src/Components/Data/Data.js
@@ -1,6 +1,7 @@
 export const projects = [
     {
       projectName: "StaySage",
+      image: 'https://i.ibb.co/LJmV0jP/hotel-booking-auth-4602f-web-app-Nest-Hub-2.png',
       description: "A user-friendly platform for booking hotel rooms with real-time availability and secure payment options.",
       coreFeatures: [
         "Real-time room availability",
@@ -8,19 +9,20 @@ export const projects = [
         "User reviews and ratings"
       ],
       technologyUsed: [
-        "React.js",
-        "Node.js",
+        "MongoDB",
         "Express.js",
-        "MongoDB"
+        "React.js",
+        "Node.js"
       ],
       liveLink: "https://hotel-booking-auth-4602f.web.app/",
       githubLinks: {
-        client: "https://github.com/user/StaySage-client",
-        server: "https://github.com/user/StaySage-server"
+        client: "https://github.com/SamyaRoy999/Stay-Sage-App",
+        server: "https://github.com/SamyaRoy999/Stay-Sage-App-server-"
       }
     },
     {
       projectName: "ArtisanalCreations",
+      image: 'https://i.ibb.co/3Mz5zxy/arts-auth-web-app-Nest-Hub.png',
       description: "An online platform dedicated to the art of painting and drawing, offering a curated collection of handcrafted masterpieces.",
       coreFeatures: [
         "User authentication with multiple login options",
@@ -28,18 +30,20 @@ export const projects = [
         "Effortless item management"
       ],
       technologyUsed: [
+        "MongoDB",
+        "Express.js",
         "React.js",
-        "Tailwind CSS",
-        "Firebase"
+        "Node.js"
       ],
       liveLink: "https://arts-auth.web.app/",
       githubLinks: {
-        client: "https://github.com/user/ArtisanalCreations-client",
-        server: "https://github.com/user/ArtisanalCreations-server"
+        client: "https://github.com/SamyaRoy999/Art-Craft-Store-client-",
+        server: "https://github.com/SamyaRoy999/Art-Craft-Store-server-"
       }
     },
     {
       projectName: "Survey-scape",
+      image: 'https://i.ibb.co/x14jrk6/survey-auth-bef1d-web-app-Nest-Hub.png',
       description: "A feature-rich platform for survey creation, voting, result analysis, and user interaction.",
       coreFeatures: [
         "Survey creation and management",
@@ -54,8 +58,8 @@ export const projects = [
       ],
       liveLink: "https://survey-auth-bef1d.web.app/",
       githubLinks: {
-        client: "https://github.com/user/Survey-scape-client",
-        server: "https://github.com/user/Survey-scape-server"
+        client: "https://github.com/SamyaRoy999/Survey-App-client-",
+        server: "https://github.com/SamyaRoy999/Survey-App-server-"
       }
     }
   ];
--- src/Components/ProjectCard/ProjectCard.jsx
+++ src/Components/ProjectCard/ProjectCard.jsx
@@ -1,46 +1,70 @@
 import { FaArrowTrendUp } from "react-icons/fa6";
 import { projects } from "../Data/Data";
 import { useState } from "react";
-console.log(projects);
+
 const PortfolioCard = ({ ditaels }) => {
-    const [modulData, setModulData] = useState(null)
-    const { coreFeatures = [], projectName, technologyUsed, liveLink, githubLinks = {}, description, } = ditaels;
-    
-    const hendelName = (name)=>{
-       const findData = projects.find(item => item.projectName === name);
-       
-       setModulData(findData)
-    }
+    const [modulData, setModulData] = useState({});
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const { coreFeatures = [], image, projectName, technologyUsed, liveLink, githubLinks = {}, description } = ditaels;
+
+    const handleName = (name) => {
+        const findData = projects.find(item => item.projectName === name);
+        setModulData(findData);
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
 
     return (
         <>
-            <div className="relative  bg-[#140C1C] p-5 rounded-md ">
-                <div className="overflow-hidden rounded-[10px]">
-                    <img className='' src='https://i.ibb.co/q0s8dG9/point3d-commercial-imaging-ltd-Swg04-CP0b-U-unsplash.jpg' alt="portfolio" />
-                    <span className="  text-white text-xl font-medium  ">
+            <div className="relative bg-[#140C1C] p-5 rounded-md">
+                <div className="overflow-hidden">
+                    <img className='h-44 w-full object-cover' src={image} alt="portfolio" />
+                    <span className="text-[#9463F7] text-xl font-medium">
                         {projectName}
                     </span>
                 </div>
-                <div className="relative text-white bg-gradient-to-br  from-[#9463F7] to-[#211138] z-10 mx-7 opacity-0 hover:opacity-100 mb-0 pb-0 duration-300 -mt-20 rounded-lg bg-white dark:bg-dark-2 py-[34px] px-3 text-center shadow-portfolio dark:shadow-box-dark">
-                    {/* <span className="text-primary mb-3 block text-sm font-medium">
-                        {projectName}
-                    </span> */}
-                    <h3 className="text-dark dark:text-white mb-4 text-xl font-bold">Creative Agency</h3>
-                    <span onClick={() =>{ document.getElementById('my_modal_4').showModal(); hendelName(projectName)}} className="flex items-center gap-3 cursor-pointer justify-center border border-b-4 border-[#9463F7] rounded hover:bg-violet-600">view details  <FaArrowTrendUp /></span>
-                    <dialog  id="my_modal_4" className="modal">
-                        <div className="modal-box w-11/12 max-w-5xl text-black">
-                            <h3 className="font-bold text-lg">Hello!</h3>
-                            <p className="py-4">Click the button below to close</p>
-                            <div className="modal-action">
-                                <form method="dialog">
-                                    {/* if there is a button, it will close the modal */}
-                                    <button className="btn">Close</button>
-                                </form>
-                            </div>
-                        </div>
-                    </dialog>
+                <div className="relative text-white bg-gradient-to-br from-[#9463F7] to-[#211138] z-10 mx-7 opacity-0 hover:opacity-100 mb-0 pb-0 duration-300 -mt-20 rounded-lg bg-white dark:bg-dark-2 py-[34px] px-3 text-center shadow-portfolio dark:shadow-box-dark">
+                    <h3 className="text-dark dark:text-white mb-4 text-xl font-bold">{projectName}</h3>
+                    <span onClick={() => handleName(projectName)} className="flex items-center gap-3 cursor-pointer justify-center border border-b-4 border-[#9463F7] rounded hover:bg-violet-600">view details <FaArrowTrendUp /></span>
                 </div>
             </div>
+
+            {isModalOpen && (
+                <div className="fixed inset-0 z-50 overflow-auto text-gray-500   bg-smoke-light flex">
+                    <div className="relative p-8  bg-[#140C1C] w-full max-w-3xl m-auto flex-col flex rounded-lg">
+                        <img src={modulData.image} alt={modulData.projectName} className="w-full font-poppins h-64 object-cover rounded-md mb-4"/>
+                        <h3 className="font-bold text-lg mb-2">{modulData.projectName}</h3>
+                        <p className="py-2 font-roboto">{modulData.description}</p>
+                        <div className="py-2">
+                            <h4 className="font-semibold">Core Features:</h4>
+                            <ul className="list-disc list-inside">
+                                {modulData.coreFeatures.map((feature, index) => (
+                                    <li key={index}>{feature}</li>
+                                ))}
+                            </ul>
+                        </div>
+                        <div className="py-2">
+                            <h4 className="font-semibold">Technology Used:</h4>
+                            <ul className="list-disc list-inside">
+                                {modulData.technologyUsed.map((tech, index) => (
+                                    <li key={index}>{tech}</li>
+                                ))}
+                            </ul>
+                        </div>
+                        <div>
+                        <h4 className="font-semibold">liveLink: <a href={liveLink} className="hover:text-[#211138]" target="_blank">{liveLink}</a></h4>
+                        <h4 className="font-semibold">github client Link: <a href={githubLinks.client} className="hover:text-[#211138]" target="_blank">{githubLinks.client}</a></h4>
+                        <h4 className="font-semibold">github server Link: <a href={githubLinks.server} className="hover:text-[#211138]" target="_blank">{githubLinks.server}</a></h4>
+                        </div>
+                        <div className="flex justify-end mt-4">
+                            <button className="btn bg-[#9463F7] text-white py-2 px-4 rounded" onClick={closeModal}>Close</button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </>
     );
 };
